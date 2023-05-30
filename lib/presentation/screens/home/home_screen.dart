180306d554fd3a3521d5299e437cb406bf2040e2
Ezequiel Rodriguez <cheke_11@hotmail.com>
2023-05-29 23:37:02 -0600

    go_router configurado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
@@ -57,7 +58,11 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
-        Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
+        /*Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const ButtonsScreen()));*/
+
+        //Navigator.pushNamed(context, menuItem.link);
       },
     );
   }
